0d6bc506c893b71b0a2496014b463935d4601dd9
Panteleyev Kirill <36451042+TreyBan@users.noreply.github.com>
2018-10-12 07:00:16 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import sqlite3
 from flask import Flask, request, json
 import vk
 
-#Переменные с клчом доступа сообщества и кодом подтверждения
+#Переменные с ключом доступа сообщества и кодом подтверждения
 token = '4445b9ec75eca61c00278e8c0ff24a2faacb108eb8551141c71b0aa7fd9385235e6eafcf25e3d0ca77459'
 confirmation_token = '209bdff8'
 
@@ -28,10 +28,26 @@ def bot(): #Главная функция
         "action": {
           "type": "text",
           "payload": "{\"button\": \"1\"}",
-          "label": "Расписание"
+          "label": "Пары сегодня"
         },
         "color": "default"
-      }]]}
+      }, {
+        "action": {
+          "type": "text",
+          "payload": "{\"button\": \"1\"}",
+          "label": "Пары завтра"
+        },
+        "color": "default"
+      }],
+      [{
+        "action": {
+          "type": "text",
+          "payload": "{\"button\": \"1\"}",
+          "label": "Следующая пара"
+        },
+        "color": "primary"
+      }]
+      ]}
 
     #Перевод клавиатуры в строку, как просит VK API
     keyboard = json.dumps(keyboard, ensure_ascii=False).encode('utf-8')
@@ -70,21 +86,27 @@ def bot(): #Главная функция
         if user_message.lower() == "начать":
             api.messages.send(user_id = str(user_id), message = 'Привет, я бот, предназначенный для отправки расписания и домашней работы 51-ой группе. Чтобы посмотреть список команд, введите "помощь"', keyboard = keyboard)
 
-        elif user_message.lower() == "расписание":
-            api.messages.send(peer_id = str(peer_id), message = raspisanie(int(hour), int(weekday)))
+        elif "пары сегодня" in user_message.lower():
+            api.messages.send(peer_id = str(peer_id), message = timetable(False))
 
-        elif "скайнет говно" in user_message.lower()  :
-            api.messages.send(peer_id = str(peer_id), message = "А еще кодом Марата можно подтереть жопу")
+        elif "следующая пара" in user_message.lower():
+            api.messages.send(peer_id = str(peer_id), message = next_class())
+
+        elif "пары завтра" in user_message.lower():
+            api.messages.send(peer_id = str(peer_id), message = timetable(True))
+
+        elif "т1000" in user_message.lower()  :
+            api.messages.send(peer_id = str(peer_id), message = "А кодом Марата можно подтереть жопу")
 
         elif user_message.lower() == "помощь":
-            text = "Список команд:\n расписание,\n домашняя работа "
+            text = "Список команд:\n пары сегодня,\n пары завтра, \n следующая пара "
             api.messages.send(peer_id = str(peer_id), message = text)
 
         elif "рэп" in user_message.lower() :
-            api.messages.send(peer_id = str(peer_id), message = 'рэп для дебилов')
+            api.messages.send(peer_id = str(peer_id), message = 'Рэп для дебилов')
 
         elif "марат" in user_message.lower():
-            api.messages.send(peer_id = str(peer_id), message = 'Марат лох')
+            api.messages.send(peer_id = str(peer_id), message = 'Марат - тупой говнокодер, все это знают.')
 
         elif user_message.lower() == "время":
             api.messages.send(peer_id = str(peer_id), message = str(hour) + ":" + str(minutes) + " " + weekday)
@@ -93,292 +115,106 @@ def bot(): #Главная функция
 
 
 
-def raspisanie(hours, weekDay): #Не смотрите на этот говнокод, пожалуйста
+def timetable(isTomorrow):
 
-    #Ну если так хочется, то вкратце эта функция возвращает строку с расписанием в зависимости от даты и времени
-
-    times = ["8:00", "9:30", "9:40", "11:10", "11:50", "13:20", "13:30", "15:00", "15:10", "16:40"]
-    classes = ["ТСИ", "Математика", "Физ-ра", "Основы программирования", "Английский", "Архитектура комп. систем", "IT", "Операционнные системы"]
-    classRooms = ["109, УЛК5", "412, УЛК2", "308, УБК", "320, УЛК2", "322, УБК", "320, УБК", "418, УЛК2", "302, УЛК2", "308, УЛК2"]
-    teachers = ["Альбина Рашидовна", "Анна Николаевна", "", "Екатерина Валерьевна", "Марьям Рифовна", "Айгуль Фларитовна", "Айрат Анварович", "Ольга Николаевна"]
-
-    if datetime.now().isocalendar()[1] % 2:
-        isHighWeek = True
+    if datetime.now().isocalendar()[1] % 2 == 1:
+        isHighWeek = 1
     else:
-        isHighWeek = False
+        isHighWeek = 0
 
-    if isHighWeek == False:
-
-        if weekDay == 0:
-            if hours <= 11: #ТСИ
-                zanyatie = classes[0]
-                time = times[4] + " - " + times[5]
-                classRoom = classRooms[0]
-                teacher = teachers[0]
-
-            elif hours >= 11 and hours <= 13: #ТСИ
-                zanyatie = classes[0]
-                time = times[6] + " - " + times[7]
-                classRoom = classRooms[1]
-                teacher = teachers[0]
-
-            elif hours >= 13 and hours <= 15: #ТСИ
-                zanyatie = classes[0]
-                time = times[8] + " - " + times[9]
-                classRoom = classRooms[1]
-                teacher = teachers[0]
-
-            else:
-                return "На сегодня пар больше нет"
-
-        elif weekDay == 1:
-            if hours <= 13: #Матан
-                zanyatie = classes[1]
-                time = times[6] + " - " + times[7]
-                classRoom = classRooms[2]
-                teacher = teachers[1]
-
-            elif hours >= 13 and hours <= 15: #Матан
-                zanyatie = classes[1]
-                time = times[8] + " - " + times[9]
-                classRoom = classRooms[3]
-                teacher = teachers[1]
-
-            else:
-                return "На сегодня пар больше нет"
-
-        elif weekDay == 2:
-            if hours <= 8: #Физра
-                zanyatie = classes[2]
-                time = times[0] + " - " + times[1]
-                classRoom = "Спорткомплекс"
-                teacher = teachers[2]
-
-            elif hours >= 8 and hours <= 9: #Инглиш
-                zanyatie = classes[4]
-                time = times[2] + " - " + times[3]
-                classRoom = classRooms[5]
-                teacher = teachers[4]
-
-            elif hours >= 9 and hours <= 11: #ОП
-                zanyatie = classes[3]
-                time = times[4] + " - " + times[5]
-                classRoom = classRooms[1]
-                teacher = teachers[3]
-
-            elif hours >= 11 and hours <= 13: #ОП
-                zanyatie = classes[3]
-                time = times[6] + " - " + times[7]
-                classRoom = classRooms[4]
-                teacher = teachers[3]
-
-            else:
-                return "На сегодня пар больше нет"
-
-        elif weekDay == 3:
-            if hours <= 8: #Архитектура КС
-                zanyatie = classes[5]
-                time = times[0] + " - " + times[1]
-                classRoom = classRooms[1]
-                teacher = teachers[5]
-
-            else:
-                return "На сегодня пар больше нет"
-
-        elif weekDay == 4:
-            if hours <= 8: #IT
-                zanyatie = classes[6]
-                time = times[0] + " - " + times[1]
-                classRoom = classRooms[6]
-                teacher = teachers[6]
-
-            elif hours >= 8 and hours <= 9: #IT
-                zanyatie = classes[6]
-                time = times[2] + " - " + times[3]
-                classRoom = classRooms[7]
-                teacher = teachers[6]
-
-            elif hours >= 9 and hours <= 11: #Матан
-                zanyatie = classes[1]
-                time = times[4] + " - " + times[5]
-                classRoom = classRooms[2]
-                teacher = teachers[1]
-
-            elif hours >= 11 and hours <= 13: #Матан
-                zanyatie = classes[1]
-                time = times[6] + " - " + times[7]
-                classRoom = classRooms[3]
-                teacher = teachers[1]
-            else:
-                return "На сегодня пар больше нет"
-
-        elif weekDay == 5:
-            if hours <= 8: #ОС
-                zanyatie = classes[7]
-                time = times[0] + " - " + times[1]
-                classRoom = classRooms[8]
-                teacher = teachers[7]
-
-            elif hours >= 8 and hours <= 9: #ОС
-                zanyatie = classes[7]
-                time = times[2] + " - " + times[3]
-                classRoom = classRooms[2]
-                teacher = teachers[7]
-
-            elif hours >= 9 and hours <= 11: #ОС
-                zanyatie = classes[7]
-                time = times[4] + " - " + times[5]
-                classRoom = classRooms[2]
-                teacher = teachers[7]
-
-            elif hours >= 11 and hours <= 13: #Архитектура КС
-                zanyatie = classes[5]
-                time = times[6] + " - " + times[7]
-                classRoom = classRooms[1]
-                teacher = teachers[5]
-            else:
-                return "На сегодня пар больше нет"
-
-        elif weekDay == 6:
-            isHighWeek = True
-            return "На сегодня пар больше нет"
+    if isTomorrow:
+        weekDay = str(datetime.today().weekday() + 2)
+        text = "Расписание на завтра: \n\n" + "*****************************" + "\n\n"
     else:
+        weekDay = str(datetime.today().weekday() + 1)
+        text = "Расписание на сегодня: \n\n" + "*****************************" + "\n\n"
 
-        if weekDay == 0:
-            if hours <= 13: #ТСИ
-                zanyatie = classes[0]
-                time = times[6] + " - " + times[7]
-                classRoom = classRooms[4]
-                teacher = teachers[0]
+    conn = sqlite3.connect('TimeTableData.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM timeTable')
+    row = cursor.fetchone()
 
-            elif hours >= 13 and hours <= 15: #ТСИ
-                zanyatie = classes[0]
-                time = times[8] + " - " + times[9]
-                classRoom = classRooms[1]
-                teacher = teachers[0]
+    while weekDay not in row[8]:
+        row = cursor.fetchone()
 
+    while weekDay in row[8]:
+        if row[3] == isHighWeek:
+            if row[6] == 1:
+                classType = "Практика "
             else:
-                return "На сегодня пар больше нет"
+                classType = "Лекция "
 
-        elif weekDay == 1:
-            if hours <= 13: #ОП
-                zanyatie = classes[3]
-                time = times[6] + " - " + times[7]
-                classRoom = classRooms[2]
-                teacher = teachers[3]
+            shedule = str(row[2]) + ", " + classType + "\n" + str(row[1]) + "\n" + "Аудитория - " + str(row[5]) + ", " + str(row[4]) + "\n" + "Препод - " + str(row[7]) + "\n\n" + "*****************************" + "\n\n"
+            text = text + shedule
+            row = cursor.fetchone()
+        else:
+            row = cursor.fetchone()
 
-            else:
-                return "На сегодня пар больше нет"
+    cursor.close()
+    conn.close()
 
-        elif weekDay == 2:
-            if hours <= 8: #Физра
-                zanyatie = classes[2]
-                time = times[0] + " - " + times[1]
-                classRoom = "Спорткомплекс"
-                teacher = teachers[2]
-
-            elif hours >= 8 and hours <= 9: #Инглиш
-                zanyatie = classes[4]
-                time = times[2] + " - " + times[3]
-                classRoom = classRooms[5]
-                teacher = teachers[4]
-
-            elif hours >= 9 and  hours <= 11: #ОП
-                zanyatie = classes[3]
-                time = times[4] + " - " + times[5]
-                classRoom = classRooms[1]
-                teacher = teachers[3]
-
-            elif hours >= 11 and hours <= 13: #ОП
-                zanyatie = classes[3]
-                time = times[6] + " - " + times[7]
-                classRoom = classRooms[4]
-                teacher = teachers[3]
-
-            else:
-                return "На сегодня пар больше нет"
-
-        elif weekDay == 3:
-            if hours <= 8: #Архитектура КС
-                zanyatie = classes[5]
-                time = times[0] + " - " + times[1]
-                classRoom = classRooms[8]
-                teacher = teachers[5]
-
-            elif hours >= 8 and hours <= 9: #Архитектура КС
-                zanyatie = classes[5]
-                time = times[2] + " - " + times[3]
-                classRoom = classRooms[0]
-                teacher = teachers[5]
-
-            elif hours >= 9 and hours <= 11: #Архитектура КС
-                zanyatie = classes[5]
-                time = times[4] + " - " + times[5]
-                classRoom = classRooms[1]
-                teacher = teachers[5]
-
-            else:
-                return "На сегодня пар больше нет"
-
-        elif weekDay == 4:
-            if hours <= 8: #IT
-                zanyatie = classes[6]
-                time = times[0] + " - " + times[1]
-                classRoom = classRooms[6]
-                teacher = teachers[6]
-
-            elif hours >= 8 and hours <= 9: #IT
-                zanyatie = classes[6]
-                time = times[2] + " - " + times[3]
-                classRoom = classRooms[7]
-                teacher = teachers[6]
-
-            elif hours >= 9 and hours <= 11: #Матан
-                zanyatie = classes[1]
-                time = times[4] + " - " + times[5]
-                classRoom = classRooms[2]
-                teacher = teachers[1]
-
-            elif hours >= 11 and hours <= 13: #Матан
-                zanyatie = classes[1]
-                time = times[6] + " - " + times[7]
-                classRoom = classRooms[3]
-                teacher = teachers[1]
-            else:
-                return "На сегодня пар больше нет"
-
-        elif weekDay == 5:
-            if hours <= 8: #ОС
-                zanyatie = classes[7]
-                time = times[0] + " - " + times[1]
-                classRoom = classRooms[8]
-                teacher = teachers[7]
-
-            elif hours >= 8 and hours <= 9: #ОС
-                zanyatie = classes[7]
-                time = times[2] + " - " + times[3]
-                classRoom = classRooms[2]
-                teacher = teachers[7]
-
-            elif hours >= 9 and hours <= 11: #ОС
-                zanyatie = classes[7]
-                time = times[4] + " - " + times[5]
-                classRoom = classRooms[2]
-                teacher = teachers[7]
-
-            elif hours >= 11 and hours <= 13: #ОС
-                zanyatie = classes[7]
-                time = times[6] + " - " + times[7]
-                classRoom = classRooms[2]
-                teacher = teachers[7]
-            else:
-                return "На сегодня пар больше нет"
-
-        elif weekDay == 6:
-            isHighWeek = False
-            return "На сегодня пар больше нет"
-
-    text = "Следующая пара - " + zanyatie + "\n" + time + "\n" + "Аудитория - " + classRoom + "\n" + "Препод - " + teacher + "\n\n"
     return text
 
+def next_class():
 
+    hour = (int(datetime.strftime(datetime.now(), "%H")) + 3) * 60
+    minutes = int(datetime.strftime(datetime.now(), "%M"))
+    weekDay = str(datetime.today().weekday() + 2)
+    nowTime = hour + minutes
+    timeList = []
+    firstClassTime = 0
+    secondClassTime = 0
+    thirdClassTime = 0
+    forthClassTime = 0
+    text = "Сегодня пар больше нет"
+
+    conn = sqlite3.connect('TimeTableData.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM timeTable')
+    row = cursor.fetchone()
+
+    if datetime.now().isocalendar()[1] % 2 == 1:
+        isHighWeek = 0
+    else:
+        isHighWeek = 1
+
+    while weekDay not in row[8]:
+        row = cursor.fetchone()
+
+    while weekDay in row[8]:
+        if row[3] == isHighWeek:
+            if row[6] == 1:
+                classType = "Практика "
+            else:
+                classType = "Лекция "
+
+            shedule = str(row[2]) + ", " + classType + "\n" + str(row[1]) + "\n" + "Аудитория - " + str(row[5]) + ", " + str(row[4]) + "\n" + "Препод - " + str(row[7]) + "\n\n"
+            timeList.append(row[1])
+            timeList.append(shedule)
+            row = cursor.fetchone()
+
+        else:
+            row = cursor.fetchone()
+
+    if timeList[0] is not None:
+        firstClassTime = int(timeList[0][0] + timeList[0][1]) * 60 + int(timeList[0][3] + timeList[0][4])
+        if nowTime < firstClassTime:
+            text = timeList[1]
+
+    if timeList[2] is not None:
+        secondClassTime = int(timeList[2][0] + timeList[2][1]) * 60 + int(timeList[2][3] + timeList[2][4])
+        if nowTime > firstClassTime and nowTime < secondClassTime:
+            text = timeList[3]
+
+    if timeList[4] is not None:
+        thirdClassTime = int(timeList[4][0] + timeList[4][1]) * 60 + int(timeList[4][3] + timeList[4][4])
+        if nowTime > secondClassTime and nowTime < thirdClassTime:
+            text = timeList[5]
+
+    if timeList[6] is not None:
+        forthClassTime = int(timeList[6][0] + timeList[6][1]) * 60 + int(timeList[6][3] + timeList[6][4])
+        if nowTime > thirdClassTime and nowTime < forthClassTime:
+            text = timeList[7]
+
+
+    return text
